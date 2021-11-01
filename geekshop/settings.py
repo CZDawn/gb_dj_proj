f234a517dd156c40f2f495627707b4d0b1c6016f
codezerodawn <codezerodawn@gmail.com>
2021-11-01 22:48:15 +0300

    Done home task for lesson 7

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -196,3 +196,16 @@ AUTHENTICATION_BACKENDS = (
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config.GOOGLE_OAUTH_CLIENT_ID
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config.GOOGLE_OAUTH_CLIENT_SECRET
+
+if os.name == 'posix':
+    CACHE_MIDDLEWARE_ALIAS = 'default'
+    CACHE_MIDDLEWARE_SECONDS = 120
+    CACHE_MIDDLEWARE_KEY_PREFIX = 'geekshop'
+    CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+        }
+    }
+
+LOW_CACHE = True
